ec9880fdf19071cf8a6761c800aa930bb7009f96
JesperBodin <112554269+JesperBodin@users.noreply.github.com>
2023-04-24 15:38:20 +0200

    Add Consumer.java

---
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -0,0 +1,13 @@
+package org.example.consumer;
+
+import org.example.service.BackendService;
+
+public class Consumer {
+
+	public static void main(String[] args) {
+
+		BackendService backendService;
+
+
+	}
+}
